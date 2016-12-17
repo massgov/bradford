0d6d039861e7661d31f56ef69a427280943b2bfd
Connor McKay <mckayconnort@gmail.com>
2016-12-16 19:32:18 -0500

    add funnel plots

--- bradford-test-dashboard/server.R
+++ bradford-test-dashboard/server.R
@@ -1,25 +1,36 @@
 library(shiny)
 
 shinyServer(function(input, output) {
-# odds plot for metrics 
+  # odds plot for metrics 
   output$odds.plot <- renderPlotly({
     limits <- aes(ymax = odds_ratio + odds_ratio_se, ymin = odds_ratio - odds_ratio_se)
-     print(ggplotly(  # print the output of ggplotly
-         ggplot(conversion.metrics, aes(x = term, y = odds_ratio)) +
-          geom_bar(stat = "identity") +
-          geom_errorbar(limits, width = 0.15, color = "grey60") +
-          theme_bw()
-       ))
-    })
+    print(ggplotly(  # print the output of ggplotly
+      ggplot(conversion.metrics, aes(x = term, y = odds_ratio)) +
+        geom_bar(stat = "identity") +
+        geom_errorbar(limits, width = 0.15, color = "grey60") +
+        theme_bw()
+    ))
+  })
   
   
   output$formstack.response.plot.global <- renderPlotly({
-    
-    dat <- data.frame(global.summary.frames[[as.numeric(input$slot.number)]])
-    breakouts <- global.breakout.frames[[as.numeric(input$slot.number)]]$loc %>%  # positions of the breakouts 
-      global.summary.frames[[as.numeric(input$slot.number)]]$timestamp[.]  # subset the date vector
+    dat <- data.frame(global.summary.frames[[as.numeric(input$global.slot.number)]])
+    breakouts <- global.breakout.frames[[as.numeric(input$global.slot.number)]]$loc %>%  # positions of the breakouts 
+      global.summary.frames[[as.numeric(input$global.slot.number)]]$timestamp[.]  # subset the date vector
     plt <- makeBreakoutPlot(dat = dat, breakouts = breakouts,
-                            x = "timestamp", y = "grand_mean")
+                            x = "timestamp", y = "prop_affirmative")
+    print(ggplotly(plt)) # print the output of ggplotly
+  }) 
+  
+  
+  output$formstack.response.plot.funnels <- renderPlotly({
+    
+    dat <- data.frame(site.summary.frames[[as.numeric(input$funnel.slot.number)]]) %>%
+      dplyr::filter(site == input$funnel.name)
+    breakouts <- site.breakout.frames[[as.numeric(input$funnel.slot.number)]]$loc %>%  # positions of the breakouts 
+      site.summary.frames[[as.numeric(input$funnel.slot.number)]]$timestamp[.]  # subset the date vector
+    plt <- makeBreakoutPlot(dat = dat, breakouts = breakouts,
+                            x = "timestamp", y = "prop_affirmative")
     print(ggplotly(plt)) # print the output of ggplotly
   }) 
 })
--- bradford-test-dashboard/ui.R
+++ bradford-test-dashboard/ui.R
@@ -5,24 +5,36 @@ shinyUI(navbarPage(
   # Application title
   title = "Bradford Test Dash",
 
-  tabPanel("Plot",
+  tabPanel("Home",
            fluidRow(splitLayout(cellWidths = c("50%", "50%"),
                                 plotlyOutput("odds.plot")
                                 # put a plot re: conversions over time here
                                 )
                     )
            ),
-  tabPanel("Summary"),
-  tabPanel("Table"),
   navbarMenu("Formstack Responses",
              tabPanel("Info"),
              "----",
              "",
-             tabPanel("Funnels"),
+             tabPanel("Funnels", 
+                      fluidRow(
+                        splitLayout(cellWidths = c("50%", "50%"),
+                          selectInput("funnel.slot.number", "Time Frame:", 
+                                      c("Weekly" = 2,
+                                        "Monthly" = 1), 
+                                      selected = 1),
+                          selectInput("funnel.name", "Funnel:",
+                                      c("Admin and Finance" = "anf",
+                                        "Courts" = "courts",
+                                        "Dept of Revenue" = "dor",
+                                        "Education" = "edu"), selected = "anf")
+                          ),
+                        plotlyOutput("formstack.response.plot.funnels")
+                      )),
              tabPanel("Endpoints"),
              tabPanel("Global", 
                       fluidRow(  
-                        selectInput("slot.number", "Time Frame:", 
+                        selectInput("global.slot.number", "Time Frame:", 
                                     c("Daily" = 1,
                                       "Weekly" = 3,
                                       "Monthly" = 2), 

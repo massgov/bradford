9932c51b7b08c0a18c18a2d21305a645d7143e82
Connor McKay <mckayconnort@gmail.com>
2016-12-29 15:12:38 -0500

    remove "<-" from within functions. add show all to analyst view

--- bradford-test-dashboard/server.R
+++ bradford-test-dashboard/server.R
@@ -4,28 +4,29 @@ shinyServer(function(input, output) {
   #### MAIN ####
   # odds plot for metrics 
   output$odds.plot <- renderPlotly({
-    limits <- aes(ymax = odds_ratio + odds_ratio_se, ymin = odds_ratio - odds_ratio_se)
+    limits = aes(ymax = odds_ratio + odds_ratio_se, ymin = odds_ratio - odds_ratio_se)
     print(ggplotly(  # print the output of ggplotly
       ggplot(conversion.metrics, aes(x = term, y = odds_ratio)) +
         geom_bar(stat = "identity") +
+        ggtitle("Best Indicators That Someone Will Not Convert") +
         geom_errorbar(limits, width = 0.15, color = "grey60") +
         theme_bw()
     ))
   })
   
   output$formstack.response.plot.global.home <- renderPlotly({
-    dat <- data.frame(global.summary.frames[[as.numeric(input$global.slot.number.home)]])
-    breakouts <- global.breakout.frames[[as.numeric(input$global.slot.number.home)]]$loc %>%  # positions of the breakouts 
+    dat = data.frame(global.summary.frames[[as.numeric(input$global.slot.number.home)]])
+    breakouts = global.breakout.frames[[as.numeric(input$global.slot.number.home)]]$loc %>%  # positions of the breakouts 
       global.summary.frames[[as.numeric(input$global.slot.number.home)]]$timestamp[.]  # subset the date vector
-    plt <- makeBreakoutPlot(dat = dat, breakouts = breakouts,
+    plt = makeBreakoutPlot(dat = dat, breakouts = breakouts,
                             x = "timestamp", y = "prop_affirmative",
                             plot.title = "Improvement in Satisfaction (Yes Rate) Over Time")
     print(ggplotly(plt)) # print the output of ggplotly
   })
   
   output$formstack.volume.plot.global.area <- renderPlotly({
-    time.unit <- c("month", "week")
-    plt <- formstack.master %>% 
+    time.unit = c("month", "week")
+    plt = formstack.master %>% 
       dplyr::mutate(timestamp = lubridate::floor_date(submit_time, 
                                                       unit = time.unit[[as.numeric(input$global.slot.number.home)]])) %>%
       dplyr::group_by(timestamp, site) %>%
@@ -36,8 +37,8 @@ shinyServer(function(input, output) {
   })
   
   output$formstack.volume.plot.home.bar <- renderPlotly({
-    time.unit <- c("month", "week")
-    plt <- formstack.master %>% 
+    time.unit = c("month", "week")
+    plt = formstack.master %>% 
       dplyr::mutate(timestamp = lubridate::floor_date(submit_time, 
                                                       unit = time.unit[[as.numeric(input$global.slot.number.home)]])) %>%
       dplyr::group_by(timestamp) %>%
@@ -49,19 +50,19 @@ shinyServer(function(input, output) {
   #### USER SATISFACTION ####
   output$formstack.response.plot.exec <- renderPlotly({
     if (input$exec.funnel.name == "show.all") { # show global stats if all is selected 
-      dat <- data.frame(global.summary.frames[[as.numeric(input$exec.slot.number)]]) 
-      breakouts <- global.breakout.frames[[as.numeric(input$exec.slot.number)]]$loc %>%  # positions of the breakouts 
+      dat = data.frame(global.summary.frames[[as.numeric(input$exec.slot.number)]]) 
+      breakouts = global.breakout.frames[[as.numeric(input$exec.slot.number)]]$loc %>%  # positions of the breakouts 
         global.summary.frames[[as.numeric(input$exec.slot.number)]]$timestamp[.]  # subset the date vector
-      plt <- makeBreakoutPlot(dat = dat, breakouts = breakouts,
+      plt = makeBreakoutPlot(dat = dat, breakouts = breakouts,
                               x = "timestamp", y = "prop_affirmative", 
                               plot.title = "Improvement in Satisfaction (Yes Rate) Over Time")
       return(print(ggplotly(plt)))
     } else {
-      dat <- data.frame(site.summary.frames[[as.numeric(input$exec.slot.number)]]) %>%
+      dat = data.frame(site.summary.frames[[as.numeric(input$exec.slot.number)]]) %>%
         dplyr::filter(site == input$exec.funnel.name)
-      breakouts <- site.breakout.frames[[as.numeric(input$exec.slot.number)]][[input$exec.funnel.name]]$loc %>%  # positions of the breakouts 
+      breakouts = site.breakout.frames[[as.numeric(input$exec.slot.number)]][[input$exec.funnel.name]]$loc %>%  # positions of the breakouts 
         site.summary.frames[[as.numeric(input$exec.slot.number)]]$timestamp[.]  # subset the date vector
-      plt <- makeBreakoutPlot(dat = dat, breakouts = breakouts,
+      plt = makeBreakoutPlot(dat = dat, breakouts = breakouts,
                               x = "timestamp", y = "prop_affirmative", 
                               plot.title = "Improvement in Satisfaction (Yes Rate) Over Time")
       return(print(ggplotly(plt))) # print the output of ggplotly
@@ -87,7 +88,7 @@ shinyServer(function(input, output) {
         dplyr::arrange(desc(Yes)) %>%
         dplyr::slice(1:10)
     }
-    plt <- dat %>%
+    plt = dat %>%
       ggplot(aes(x = reorder(content_author, Yes), y = Yes)) +
       geom_bar(stat = "identity") +
       coord_flip() +
@@ -117,7 +118,7 @@ shinyServer(function(input, output) {
         dplyr::arrange(desc(Yes)) %>%
         dplyr::slice(1:10)
     }
-    plt <- dat %>%
+    plt = dat %>%
       ggplot(aes(x = reorder(os, Yes), y = Yes)) +
       geom_bar(stat = "identity") +
       ggtitle("User Satisfaction Across OS - Top 10") +
@@ -255,41 +256,70 @@ shinyServer(function(input, output) {
   
   #### ANALYST - USER SATISFACTION FUNNELS ####
   output$formstack.response.plot.funnels <- renderPlotly({
-    dat <- data.frame(site.summary.frames[[as.numeric(input$funnel.slot.number)]]) %>%
-      dplyr::filter(site == input$funnel.name)
-    breakouts <- site.breakout.frames[[as.numeric(input$funnel.slot.number)]][[input$funnel.name]]$loc %>%  # positions of the breakouts 
-      site.summary.frames[[as.numeric(input$funnel.slot.number)]]$timestamp[.]  # subset the date vector
-    plt <- makeBreakoutPlot(dat = dat, breakouts = breakouts,
-                            x = "timestamp", y = "prop_affirmative")
-    print(ggplotly(plt)) # print the output of ggplotly
+    
+    if (input$funnel.name == "show.all") {
+      dat <- data.frame(global.summary.frames[[as.numeric(input$funnel.slot.number)]]) 
+      breakouts <- global.breakout.frames[[as.numeric(input$funnel.slot.number)]]$loc %>%  # positions of the breakouts 
+        global.summary.frames[[as.numeric(input$funnel.slot.number)]]$timestamp[.]  # subset the date vector
+      plt <- makeBreakoutPlot(dat = dat, breakouts = breakouts,
+                              x = "timestamp", y = "prop_affirmative", 
+                              plot.title = "Improvement in Satisfaction (Yes Rate) Over Time")
+      return(print(ggplotly(plt)))
+    } else {
+      dat <- data.frame(site.summary.frames[[as.numeric(input$funnel.slot.number)]]) %>%
+        dplyr::filter(site == input$funnel.name)
+      breakouts <- site.breakout.frames[[as.numeric(input$funnel.slot.number)]][[input$funnel.name]]$loc %>%  # positions of the breakouts 
+        site.summary.frames[[as.numeric(input$funnel.slot.number)]]$timestamp[.]
+      plt <- makeBreakoutPlot(dat = dat, breakouts = breakouts,
+                              x = "timestamp", y = "prop_affirmative")
+      return(print(ggplotly(plt))) # print the output of ggplotly
+    }
   }) 
   
   output$formstack.volume.plot.funnel.bar <- renderPlotly({
-    time.unit <- c("month", "week")
-    plt <- formstack.master %>% 
-      dplyr::filter(site == input$funnel.name) %>%
-      dplyr::mutate(timestamp = lubridate::floor_date(submit_time, 
-                                                      unit = time.unit[[as.numeric(input$funnel.slot.number)]])) %>%
-      dplyr::group_by(timestamp) %>%
-      dplyr::count() %>%
-      makeVolumeBarPlot(x = "timestamp", y = "n", ylab = "Response Count")
+    time.unit <- c("month", "week") # time unit for flooring dates 
+    if (input$funnel.name == "show.all") {
+      dat = formstack.master %>% 
+        dplyr::mutate(timestamp = lubridate::floor_date(submit_time, 
+                                                        unit = time.unit[[as.numeric(input$funnel.slot.number)]])) %>%
+        dplyr::group_by(timestamp) %>%
+        dplyr::count()
+    } else {
+      dat = formstack.master %>% 
+        dplyr::filter(site == input$funnel.name) %>%
+        dplyr::mutate(timestamp = lubridate::floor_date(submit_time, 
+                                                        unit = time.unit[[as.numeric(input$funnel.slot.number)]])) %>%
+        dplyr::group_by(timestamp) %>%
+        dplyr::count()
+    }
+    plt = makeVolumeBarPlot(df = dat, x = "timestamp", y = "n", ylab = "Response Count")
     print(ggplotly(plt))  
   })
   
   output$formstack.volume.plot.funnel.endpoints <- renderPlotly({
-    plt <- formstack.master  %>% 
-      dplyr::filter(site == input$funnel.name) %>%
-      dplyr::group_by(content_author) %>%  # can also be enpoint (should be?)
-      dplyr::summarise(n = n(),
-                       Yes = (sum(info_found == "Yes") / n) * 100,
-                       No = sum(info_found == "No") / n) %>%
-      dplyr::arrange(desc(n)) %>%
-      dplyr::slice(1:10) %>%
+    if (input$funnel.name == "show.all") {
+      dat = formstack.master %>%
+        dplyr::group_by(content_author) %>%  # can also be enpoint (should be?)
+        dplyr::summarise(n = n(),
+                         Yes = (sum(info_found == "Yes") / n) * 100,
+                         No = sum(info_found == "No") / n) %>%
+        dplyr::arrange(desc(n)) %>%
+        dplyr::slice(1:10)
+    } else {
+      dat = formstack.master  %>% 
+        dplyr::filter(site == input$funnel.name) %>%
+        dplyr::group_by(content_author) %>%  # can also be enpoint (should be?)
+        dplyr::summarise(n = n(),
+                         Yes = (sum(info_found == "Yes") / n) * 100,
+                         No = sum(info_found == "No") / n) %>%
+        dplyr::arrange(desc(n)) %>%
+        dplyr::slice(1:10)
+    }
+    plt = dat %>%
       ggplot(aes(x = reorder(content_author, n), y = n, fill = Yes)) +
       geom_bar(stat = "identity") +
       coord_flip() +
       scale_fill_continuous(name = "% Found <br>Content") +
-      #guides(fill = F) +
       xlab("") +
       ylab("") +
       ggtitle("Endpoint Response Count") +
@@ -298,14 +328,25 @@ shinyServer(function(input, output) {
   })
   
   output$formstack.os.plot.funnel <- renderPlotly({
-    plt <- formstack.master %>%
-      dplyr::filter(site == input$funnel.name) %>%
-      dplyr::group_by(os) %>%
-      dplyr::summarise(n = n(),
-                       Yes = (sum(info_found == "Yes") / n) * 100,
-                       No = sum(info_found == "No") / n) %>%
-      dplyr::arrange(desc(n)) %>%
-      dplyr::slice(1:10) %>%
+    if (input$funnel.name == "show.all") {
+      dat = formstack.master %>%
+        dplyr::group_by(os) %>%
+        dplyr::summarise(n = n(),
+                         Yes = (sum(info_found == "Yes") / n) * 100,
+                         No = sum(info_found == "No") / n) %>%
+        dplyr::arrange(desc(n)) %>%
+        dplyr::slice(1:10)
+    } else {
+      dat = formstack.master %>%
+        dplyr::filter(site == input$funnel.name) %>%
+        dplyr::group_by(os) %>%
+        dplyr::summarise(n = n(),
+                         Yes = (sum(info_found == "Yes") / n) * 100,
+                         No = sum(info_found == "No") / n) %>%
+        dplyr::arrange(desc(n)) %>%
+        dplyr::slice(1:10)
+    }
+    plt = dat %>%
       ggplot(aes(x = reorder(os, n), y = n, fill = Yes)) +
       geom_bar(stat = "identity") +
       ggtitle("OS, Top 10") +
@@ -318,10 +359,17 @@ shinyServer(function(input, output) {
   })  
   
   output$formstack.affirmative.plot.funnel <- renderPlotly({
-    plt <- formstack.master %>% 
-      dplyr::filter(site == input$funnel.name) %>%
-      dplyr::group_by(info_found) %>%
-      dplyr::count() %>%
+    if (input$funnel.name == "show.all") {
+      dat = formstack.master %>% 
+        dplyr::group_by(info_found) %>%
+        dplyr::count()
+    } else {
+      dat = formstack.master %>% 
+        dplyr::filter(site == input$funnel.name) %>%
+        dplyr::group_by(info_found) %>%
+        dplyr::count()
+    }
+    plt = dat %>%
       makeAffirmativeBarPlot(x = "info_found", y = "n", plot.title = "Info Found?")
     print(ggplotly(plt))
   })

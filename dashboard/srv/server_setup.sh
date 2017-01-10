92cbbaba3eb10ea63e4bf9b10dfce9a058612cbd
Connor McKay <mckayconnort@gmail.com>
2017-01-10 13:59:26 -0500

    final touches to respect new shiny-server version

--- dashboard/srv/server_setup.sh
+++ dashboard/srv/server_setup.sh
@@ -13,7 +13,6 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
 sudo apt-get install r-base
 sudo apt-get install r-cran-xml libcurl4-gnutls-dev libxml2-dev
 sudo apt-get install openssl
-sudo apt-get install libssl0.9.8
 sudo apt-get install gdebi-core
 sudo apt-get install jq
 wget -O ~/shiny-server-1.5.1.834-amd64.deb https://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.5.1.834-amd64.deb
@@ -21,6 +20,8 @@ sudo gdebi ~/shiny-server-1.5.1.834-amd64.deb
 
 # Install postgres
 sudo apt-get install postgresql postgresql-contrib libpq-dev
+
+git clone https://github.com/massgov/bradford ~/
 
 # Install packages
 declare -a packages=('shiny'
@@ -43,15 +44,17 @@ declare -a packages=('shiny'
                      )
 
 for package_name in "${packages[@]}"; do
-  echo "installing $package_name"
   sudo su - -c "R -e \"install.packages('$package_name', repos='https://cran.rstudio.com/')\""
 done
 
 # restart the server
-sudo restart shiny-server
+sudo systemctl restart shiny-server
 
 sudo mkdir /srv/shiny-server/bradford
 sudo mkdir /srv/shiny-server/bradford/data
 
 # hack per https://github.com/ropensci/plotly/issues/494 to get shiny + plotly to play nicely
-chown -R shiny:shiny /srv/shiny-server
+sudo chown -R shiny:shiny /srv/shiny-server
+
+# move the dashboard to the shiny-server directory
+sudo cp -r ~/bradford/dashboard/* /srv/shiny-server/bradford

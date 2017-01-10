5ab2192047d6934161565a7f577daa7830e9a648
Connor McKay <mckayconnort@gmail.com>
2017-01-10 13:20:23 -0500

    add reference to proper libssl

--- dashboard/srv/server_setup.sh
+++ dashboard/srv/server_setup.sh
@@ -6,11 +6,14 @@ sudo apt-get dist-upgrade
 # Add new R CRAN
 sudo echo "deb http://cran.rstudio.com/bin/linux/ubuntu trusty/" >> /etc/apt/sources.list
 
+# Add ssl reference for install
+sudo echo "deb http://security.ubuntu.com/ubuntu lucid-security main" >> /etc/apt/sources.list
+
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
 sudo apt-get install r-base
 sudo apt-get install r-cran-xml libcurl4-gnutls-dev libxml2-dev
 sudo apt-get install openssl
-sudo apt-get install libssl-dev
+sudo apt-get install libssl0.9.8
 sudo apt-get install gdebi-core
 sudo apt-get install jq
 wget -O ~/shiny-server-1.4.1.759-amd64.deb https://download3.rstudio.org/ubuntu-12.04/x86_64/shiny-server-1.4.1.759-amd64.deb

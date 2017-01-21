343cae9c19547f8c4b310dc4b6209ec67013aae7
comckay <mckayconnort@gmail.com>
2017-01-21 16:31:54 -0500

    better printing for linter

--- dashboard/tests/test_lint.R
+++ dashboard/tests/test_lint.R
@@ -4,8 +4,10 @@ library(lintr)
 if (requireNamespace("lintr", quietly = TRUE)) {
   context("lints")
   test_that("Code is Lint Free", {
-    x <- list.files(path = "../../", pattern = ".R$", full.names = T, recursive = T) %>%
-      purrr::map_df(function(x) lintr::lint(x) %>% data.frame())
+    x <- list.files(path = "../../", pattern = ".R$",
+                    full.names = T, recursive = T) %>%
+      purrr::map_df(function(x) lintr::lint(x) %>% data.frame()) %>%
+      dplyr::select(filename, line_number, message)
     print(x)
     expect_equal(nrow(x), 0)
   })

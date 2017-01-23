ebbb06aa8a43654a713e643c8bfaeae7b1b4e774
Connor McKay <mckayconnort@gmail.com>
2017-01-23 13:47:50 -0500

    add other string method

--- dashboard/srv/test_branch.sh
+++ dashboard/srv/test_branch.sh
@@ -17,7 +17,7 @@ else
   git checkout $branch
   git pull || exit 255
 fi
-append_dir = "_TEST"
-sudo cp -r ~/$dir_name/* /srv/shiny-server/${dir_name}${append_dir}/
+test_dir = $dir_name"_TEST"
+sudo cp -r ~/$dir_name/* /srv/shiny-server/$test_dir/
 # restart the server
 sudo restart shiny-server

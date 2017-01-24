129aab8db14a4594c3bfca1763c6108b885795e7
Connor McKay <mckayconnort@gmail.com>
2017-01-24 15:21:03 -0500

    lint plot tests

--- dashboard/tests/test_make_plots.R
+++ dashboard/tests/test_make_plots.R
@@ -3,10 +3,9 @@ source("../functions/make_plots.R")
 
 context("Plotting helpers")
 
-test_that("printGGplotly errors on incorrect input" ,{
+test_that("printGGplotly errors on incorrect input", {
   x <- runif(100)
   y <- runif(100)
   plt <- plot(x = x, y = y)
   expect_error(printGGplotly(plt))
                })
-  

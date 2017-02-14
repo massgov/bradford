062273fdab9864081bbe854746b4ae8e6f76c252
Connor McKay <mckayconnort@gmail.com>
2017-02-13 19:34:18 -0500

    fix weirdness with aws cli

--- dashboard/srv/test_branch.sh
+++ dashboard/srv/test_branch.sh
@@ -19,7 +19,7 @@ else
 fi
 
 # get the connection creds
-sudo /home/ubuntu/.local/bin/aws s3 cp s3://mass.gov-analytics/dashboards/bradford/query_creds/db_connect.R ~/bradford/dashboard/get_data/
+sudo aws s3 cp s3://mass.gov-analytics/dashboards/bradford/query_creds/db_connect.R ~/bradford/dashboard/get_data/
 
 # run the query so we have up to date data
 Rscript ~/bradford/dashboard/get_data/query.R
